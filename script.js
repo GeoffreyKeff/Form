2737f7bbb69a277191ef9149ccb9518b0af983a7
Geoffrey KEFF <geoffrey.keff@viacesi.fr>
2023-04-04 19:13:10 +0200

    commentary

--- script.js
+++ script.js
@@ -1,43 +1,51 @@
 $(function() {
+
+    // Initialize variables
     var fieldIndex = 1;
     var formFields = $('#form-fields');
     var formTable = $('#form-table tbody');
 
+    // Function to update the table with the form fields
     function updateTable() {
         formTable.empty();
         formFields.find('.form-field').each(function() {
             var name = $(this).find('input[name="text-field[]"]').val() || '';
             var age = $(this).find('select[name="age[]"] option:selected').text() || '';
-            var row = '<tr><td>' + name + '</td><td>' + age + '</td><td><button class="remove-field">Supprimer</button></td></tr>';
+            var row = '<tr><td>' + name + '</td><td>' + age + '</td><td><button class="remove-field">Remove</button></td></tr>';
             formTable.append(row);
         });
     }
 
+    // Add a text field to the form
     $('#add-text-field').click(function() {
-        var newField = '<div class="form-field"><input type="text" name="text-field[]" placeholder="Prénom"><button class="remove-field">Supprimer</button></div>';
+        var newField = '<div class="form-field"><input type="text" name="text-field[]" placeholder="First name"><button class="remove-field">Remove</button></div>';
         formFields.append(newField);
         fieldIndex++;
     });
 
+    // Add a select field to the form
     $('#add-select-field').click(function() {
-        var newField = '<div class="form-field"><select name="age[]"><option value="">-- Sélectionnez une catégorie d\'âge --</option><option value="0-17 ans">0-5 ans</option><option value="0-17 ans">6-12 ans</option><option value="0-17 ans">13-17 ans</option><option value="18-25 ans">18-25 ans</option><option value="26-35 ans">26-35 ans</option><option value="36-45 ans">36-45 ans</option><option value="46-55 ans">46-55 ans</option><option value="56 ans et plus">56 ans et plus</option></select><button class="remove-field">Supprimer</button></div>';
+        var newField = '<div class="form-field"><select name="age[]"><option value="">-- Select an age category --</option><option value="0-17 years">0-5 years</option><option value="0-17 years">6-12 years</option><option value="0-17 years">13-17 years</option><option value="18-25 years">18-25 years</option><option value="26-35 years">26-35 years</option><option value="36-45 years">36-45 years</option><option value="46-55 years">46-55 years</option><option value="56 years and over">56 years and over</option></select><button class="remove-field">Remove</button></div>';
         formFields.append(newField);
         fieldIndex++;
     });
 
+    // Remove a form field
     formFields.on('click', '.remove-field', function() {
         $(this).closest('.form-field').remove();
     });
 
+    // Remove a row from the table
     formTable.on('click', '.remove-field', function() {
         $(this).closest('tr').remove();
     });
 
+    // Submit the form and add the form data to the table
     $('#dynamic-form').submit(function(e) {
         e.preventDefault();
         var name = $('input[name="text-field[]"]').last().val() || '';
         var age = $('select[name="age[]"]').last().find('option:selected').text() || '';
-        var row = '<tr><td>' + name + '</td><td>' + age + '</td><td><button class="remove-field">Supprimer</button></td></tr>';
+        var row = '<tr><td>' + name + '</td><td>' + age + '</td><td><button class="remove-field">Remove</button></td></tr>';
         formTable.append(row);
         $(this)[0].reset();
     });
